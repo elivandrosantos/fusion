a21bf349ee3424b2b0c03a9701a38a9667c95dc4
Elivandro Santos <elivandrocsantos@gmail.com>
2023-02-25 19:28:04 -0300

    update dois

--- fusion/settings.py
+++ fusion/settings.py
@@ -28,9 +28,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = secrets.token_hex(32)
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 
 # Application definition
