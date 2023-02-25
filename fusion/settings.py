5b49eae8f5ba5e72da35ffe57e33d660d1aaf386
Elivandro Santos <elivandrocsantos@gmail.com>
2023-02-25 20:01:19 -0300

    update one

--- fusion/settings.py
+++ fusion/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = secrets.token_hex(32)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['ordnavile-production-a57b.up.railway.app/', ]
+ALLOWED_HOSTS = ['.up.railway.app']
 
 
 # Application definition
