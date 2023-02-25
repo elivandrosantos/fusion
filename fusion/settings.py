c54ba7501189045d8ee111d4b5f4fc7aaea5adea
Elivandro Santos <elivandrocsantos@gmail.com>
2023-02-25 18:07:25 -0300

    Update settings.py

--- fusion/settings.py
+++ fusion/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = secrets.token_hex(32)
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
