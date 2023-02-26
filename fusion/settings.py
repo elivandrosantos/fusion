1a9ec63cda5e5ebbae0e145c87195168cf7f4b97
Elivandro Santos <elivandrocsantos@gmail.com>
2023-02-25 22:41:56 -0300

    update

--- fusion/settings.py
+++ fusion/settings.py
@@ -24,11 +24,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY=config('SECRET_KEY')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG', default=False, cast=bool)
+DEBUG=config('DEBUG', default=False, cast=bool)
 
 # ALLOWED_HOSTS = ['.up.railway.app']
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', default=[], cast=Csv())
