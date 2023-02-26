e0572ffde79289dc89e3879276293961daa7c656
Elivandro Santos <elivandrocsantos@gmail.com>
2023-02-25 21:11:52 -0300

    update

--- fusion/settings.py
+++ fusion/settings.py
@@ -10,10 +10,10 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
+from decouple import Csv, config
 from pathlib import Path
-import secrets
 import os
-import dj_database_url
+from dj_database_url import parse as dburl
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,12 +25,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 
-SECRET_KEY = secrets.token_hex(32)
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
 ALLOWED_HOSTS = ['.up.railway.app']
+# ALLOWED_HOSTS = config('ALLOWED_HOSTS', default=[], cast=Csv())
 
 
 # Application definition
@@ -57,7 +58,7 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = "fusion.urls"
 
-TEMPLATES_DIRS = os.path.join('templates')
+TEMPLATES_DIRS = BASE_DIR.joinpath('templates')
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
